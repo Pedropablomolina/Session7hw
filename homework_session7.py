d02ad8cd5147145410f7f12b25b9fdd055b3a335
pedropablomolina <ppmolinaarol@gmail.com>
2023-03-13 15:31:35 +0100

    .

--- homework_session7.py
+++ homework_session7.py
@@ -4,3 +4,15 @@ print(r.text)
 a = r.text
 alphabet = " abcdefghijklmnopqrstuvwxyz"
 vowels = "aeiou"
+words = a.split('\n')
+message = ""
+for word in words:
+    vowel_count = 0
+    for char in word:
+        if char in vowels:
+            vowel_count += 1
+    message += alphabet[vowel_count]
+
+print(message)
+
+
